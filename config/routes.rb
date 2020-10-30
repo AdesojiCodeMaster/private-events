88c7eba58223b53bedf072eae3814ad42d9011ea
Juan <jacobo12.montes@gmail.com>
2020-10-30 12:36:49 -0500

    Add the users and events models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :events
+  get 'users/new'
+  get 'users/create'
+  get 'users/show'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "users#index"
+  
 end
