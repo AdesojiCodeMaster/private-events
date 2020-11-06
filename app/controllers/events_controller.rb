fd9836d6b53b50b54939f3f62b5fe795d773eca8
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-11-06 02:06:03 +0100

    Add more styling and resetting the routes to avoid errors

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -52,6 +52,7 @@ class EventsController < ApplicationController
   # PATCH/PUT /events/1
   # PATCH/PUT /events/1.json
   def update
+    @event = Event.find(params[:id])
     respond_to do |format|
       if @event.update(event_params)
         format.html { redirect_to @event, notice: 'Event was successfully updated.' }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :event_attendances
-  resources :events, only: %i[index create new show edit attending] do
+  resources :events, only: %i[index create new show edit attending update] do
     post 'attending', :users
   end
   get 'users/new'
