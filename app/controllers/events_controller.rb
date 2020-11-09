f2f83c4f96af0589083e7259ee0109b78d4213c3
Juan <jacobo12.montes@gmail.com>
2020-11-09 17:20:01 -0500

    Fix the linter errors

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,7 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
     @upcoming_events = Event.upcoming_events
-    @past_events = Event.past_events  
+    @past_events = Event.past_events
   end
 
   # GET /events/1
