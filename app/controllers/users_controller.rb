abaa544a1ff09a517a9d9393a42a7cd9cb0083d4
Juan <jacobo12.montes@gmail.com>
2020-11-09 17:17:48 -0500

    Add the past and upcomming events to the index method on the events controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,13 @@ class UsersController < ApplicationController
   # user controller actions
   def index
     @users = User.all
+    @created_events = current_user.events
+    @past_events = current_user.attended_events.previous
+    @upcoming_events = current_user.attended_events.upcoming
   end
 
   def show
     @user = User.find(current_user.id)
+
   end
 end
