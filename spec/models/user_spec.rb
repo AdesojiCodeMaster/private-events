5564a7a424bcbcb2fae19b53ad2b398bc4a03ba8
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-11-14 09:57:24 +0100

    Add user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Create a new user' do
+    
+    it 'it should create a user with both attributes' do
+      x = User.new(name: 'Juan', email: 'juanu@example.com')
+      expect(x.valid?).to eql(false)
+    end
+  end
+
 end
